8aaf1989d27cec02394ed4db8515615ff1567771
Raul200220 <lurams05@gmail.com>
2026-02-14 11:57:49 -0600

    Primer Commit

---
+++ src/main/java/com/mycompany/proyectoestructuradatos/ProyectoEstructuraDatos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoestructuradatos;
+
+/**
+ *
+ * @author raulb
+ * 1.	Avance 1: GRUPO_5_PFA1. En caso de no cumplir con esta instrucción se descontarán 2 puntos de su sumatoria final.
+ * 2.	Avance 2: GRUPO_5_PFA2. En caso de no cumplir con esta instrucción se descontarán 2 puntos de su sumatoria final.
+ * 3.	Presentación Final: GRUPO_5_PFAF. En caso de no cumplir con esta instrucción se descontarán 2 puntos de su sumatoria final.
+
+ */
+public class ProyectoEstructuraDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
